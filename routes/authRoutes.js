820ff4801a2890e7689e87bad40c94e4a21e2fae
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-01-16 08:10:58 +0330

    ADD Get,Update,Delete user & login routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,28 @@
 const express = require('express');
 
-const { createUser } = require('../controllers/userController');
+const { auth } = require('../middlewares/authMiddleware');
+
+const {
+  createUser,
+  userLogin,
+  getAllUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+} = require('../controllers/userController');
+
 const router = express.Router();
 
+router.get('/usersList', getAllUsers);
+
+router.get('/:id', auth, getUser);
+
 router.post('/register', createUser);
+
+router.post('/login', userLogin);
+
+router.patch('/:id', updateUser);
+
+router.patch('/delete/:id', deleteUser);
 
 module.exports = router;
