98534be23b63e163f43206f10631da8fd333befb
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-01-16 08:13:18 +0330

    ADD get,update,delete, login user & get users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const generateToken = require('../config/jwtToken');
 const User = require('../models/userModel');
 const asynchandler = require('express-async-handler');
 
@@ -17,6 +18,94 @@ const createUser = asynchandler(async (req, res, next) => {
   }
 });
 
+const userLogin = asynchandler(async (req, res, next) => {
+  const { password, email } = req.body;
+
+  //Check User Existance
+  const user = await User.findOne({ email });
+  const isMatched = await user.isPasswordMatched(password);
+
+  if (user && isMatched) {
+    res.json({
+      id: user?._id,
+      firstName: user?.firstName,
+      lastName: user?.lastName,
+      email: user?.email,
+      mobile: user?.mobile,
+      token: generateToken(user?._id),
+    });
+  } else {
+    throw new Error('Invalid Credentials');
+  }
+});
+
+const getAllUsers = asynchandler(async (req, res, next) => {
+  try {
+    const users = await User.find({ softDelete: false });
+    if (users.length) {
+      res.json(users);
+    } else {
+      res.json('Users Not Found');
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getUser = asynchandler(async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findOne({ _id: id, softDelete: false });
+    if (user) {
+      res.json({ user });
+    } else {
+      res.json('User Not Found');
+    }
+  } catch {
+    throw new Error(error);
+  }
+});
+
+const updateUser = asynchandler(async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const updatedUser = await User.findOneAndUpdate({ _id: id, softDelete: false }, req.body, {
+      new: true,
+    });
+    if (updatedUser) {
+      res.json({
+        user: updatedUser,
+      });
+    } else {
+      res.json('User Not Found');
+    }
+  } catch {
+    throw new Error(error);
+  }
+});
+
+const deleteUser = asynchandler(async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndUpdate(id, { softDelete: true }, { new: true });
+    if (user) {
+      res.json({
+        id: user?._id,
+        message: 'User Deleted Successfully',
+      });
+    } else {
+      res.json('User Not Found');
+    }
+  } catch {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createUser,
+  userLogin,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
 };
