3223149651dfbfa738b44bddc28992fddff8e9dc
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-01-27 08:58:53 +0330

    ADD generateRefreshToken function

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -1,8 +1,11 @@
 const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv').config();
 
 const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.SECRET_KEY, { expiresIn: '30 days' });
+  return jwt.sign({ userId }, process.env.SECRET_KEY, { expiresIn: '30m' });
 };
 
-module.exports = generateToken;
+const generateRefreshToken = (userId) => {
+  return jwt.sign({ userId }, process.env.REF_SECRET_KEY, { expiresIn: '2h' });
+};
+
+module.exports = { generateToken, generateRefreshToken };
