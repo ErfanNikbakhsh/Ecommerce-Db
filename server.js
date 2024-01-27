9885b8df2e1adf3db4b28846805e73a28cc1e391
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-01-27 11:55:07 +0330

    ADD /api/product route

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const dbConnect = require('./config/dbConnect');
-const authRouter = require('./routes/authRoutes');
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
 const { errorHandler, notFound } = require('./middlewares/errorHandler');
 
 const PORT = process.env.PORT || 4000;
@@ -15,7 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/user', authRouter);
+app.use('/api/user', authRoutes);
+app.use('/api/product', productRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
