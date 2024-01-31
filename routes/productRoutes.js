6e930fd8e774d0e5fca704b521d2cb31758a4918
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-01-31 09:57:35 +0330

    DEFINE create, update & delete product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
-const { createProduct, getProduct, getAllProducts } = require('../controllers/productController');
+const {
+  createProduct,
+  getProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+} = require('../controllers/productController');
+const { isAdmin, auth } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
@@ -8,6 +15,10 @@ router.get('/List', getAllProducts);
 
 router.get('/:id', getProduct);
 
-router.post('/', createProduct);
+router.post('/', auth, isAdmin, createProduct);
+
+router.patch('/:id', auth, isAdmin, updateProduct);
+
+router.patch('/delete/:id', auth, isAdmin, deleteProduct);
 
 module.exports = router;
