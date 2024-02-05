18c71216d820587b27248096a9102cb9e0a095ba
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-02-05 21:51:55 +0330

    DEFINE the sendEmail function

---
+++ utils/email.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (option) => {
+  // Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      ciphers: 'SSLv3',
+    },
+  });
+
+  // Define email options
+  const emailOptions = {
+    from: 'Digitic support<support@digitic.com>',
+    to: option.email,
+    subject: option.subject,
+    text: option.message,
+    html: option.htmlToSend,
+  };
+
+  // Why do we need the await keyword when sending the email?
+  await transporter.sendMail(emailOptions);
+};
+
+module.exports = { sendEmail };
