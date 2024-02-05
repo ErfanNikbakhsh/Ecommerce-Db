d7a12e6260c02d507cef07e072bcdbf930f0147b
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-02-05 21:50:28 +0330

    DEFINE /validateResetToken, /forgotPassword & /resetPassword routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
-const { auth, isAdmin, refreshToken } = require('../middlewares/authMiddleware');
+const {
+  auth,
+  isAdmin,
+  refreshToken,
+  forgotPassword,
+  resetPassword,
+  validateResetToken,
+} = require('../middlewares/authMiddleware');
 
 const {
   createUser,
@@ -22,15 +29,21 @@ router.get('/:id', auth, isAdmin, getUser);
 
 router.get('/refreshToken', refreshToken);
 
+router.get('/validateResetToken/:token', validateResetToken);
+
 router.post('/register', createUser);
 
 router.post('/login', userLogin);
 
-router.post('/logout', userLogout);
+router.post('/forgotPassword', forgotPassword);
+
+router.post('/logout', auth, userLogout);
 
 router.patch('/edit', auth, updateUser);
 
 router.patch('/delete', auth, deleteUser);
+
+router.patch('/resetPassword/:token', resetPassword);
 
 router.patch('/blockUser/:id', auth, isAdmin, blockUser);
 
