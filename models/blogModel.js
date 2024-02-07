04ce3ab2bd7953e3679b8c9d1d22ba11cca5cc1c
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-02-07 10:05:11 +0330

    DEFINE the blog model

---
+++ models/blogModel.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    totalViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    disLikes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    image: {
+      type: String,
+      default: '',
+    },
+    author: {
+      type: String,
+      default: 'admin',
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
