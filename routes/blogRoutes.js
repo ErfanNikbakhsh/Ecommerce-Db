ae1b2e27bf8a9fb9fae0a53119ec1e031e529b8b
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-02-13 08:21:13 +0330

    DEFINE CRUD routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const { isAdmin, auth } = require('../middlewares/authMiddleware');
-const { createBlog } = require('../controllers/blogController');
+const {
+  createBlog,
+  updateBlog,
+  getBlog,
+  getAllBlogs,
+  deleteBlog,
+} = require('../controllers/blogController');
 
 const router = express.Router();
 
+router.get('/list', getAllBlogs);
+
+router.get('/:id', getBlog);
+
 router.post('/', auth, isAdmin, createBlog);
+
+router.patch('/delete/:id', auth, isAdmin, deleteBlog);
+
+router.patch('/:id', auth, isAdmin, updateBlog);
 
 module.exports = router;
