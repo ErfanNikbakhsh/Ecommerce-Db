64d86a1a8f722528c1804993576d17d1221cd6d1
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-02-18 09:04:56 +0330

    DEFINE brand routes

---
+++ routes/brandRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const {
+  createBrand,
+  updateBrand,
+  deleteBrand,
+  getBrand,
+  getAllProdCategories,
+} = require('../controllers/brandController');
+const { isAdmin, auth } = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.get('/list', auth, isAdmin, getAllProdCategories);
+
+router.get('/:id', auth, isAdmin, getBrand);
+
+router.post('/', auth, isAdmin, createBrand);
+
+router.patch('/delete/:id', auth, isAdmin, deleteBrand);
+
+router.patch('/:id', auth, isAdmin, updateBrand);
+
+module.exports = router;
