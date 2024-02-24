37866ef07ba9b8b42c3954de8d6b6d803669739a
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-02-24 15:00:56 +0330

    DEFINE resizeAndSaveImage function

--- utils/Api-Features.js
+++ utils/Api-Features.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const sharp = require('sharp');
+const fse = require('fs-extra');
 const { createHash } = require('node:crypto');
 const ObjectId = mongoose.Types.ObjectId;
 
@@ -18,4 +20,27 @@ const hashToken = (token) => {
   return createHash('sha256').update(token).digest('hex');
 };
 
-module.exports = { logMiddleware, isObjectIdValid, hashToken };
+const resizeAndSaveImage = async (files, outputFolder) => {
+  if (!files) return;
+
+  const resizedFilesPath = await Promise.all(
+    files.map(async (file) => {
+      const outputPath = `public/images/${outputFolder}/${file.filename}`;
+
+      await sharp(file.path)
+        .resize(300, 300)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(outputPath);
+
+      // Delete the original file after successful resize
+      await fse.remove(file.path);
+
+      return outputPath;
+    })
+  );
+
+  return resizedFilesPath;
+};
+
+module.exports = { logMiddleware, isObjectIdValid, hashToken, resizeAndSaveImage };
