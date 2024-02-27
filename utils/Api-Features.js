f7b44d919257e20b9a52a10c9d3bfbd15440fd54
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-02-27 14:48:45 +0330

    DEBUG: fix the isObjectIdValid function's if condition

--- utils/Api-Features.js
+++ utils/Api-Features.js
@@ -12,7 +12,7 @@ const logMiddleware = (name) => {
 };
 
 const isObjectIdValid = (id) => {
-  if (!typeof id === Object) {
+  if (typeof id !== Object) {
     const isValid = ObjectId.isValid(id) && String(new ObjectId(id)) === id;
     if (!isValid) throw new Error('Id Is Not Valid');
   }
