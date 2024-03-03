b8b2f0463f316140fa8339b046a691257a4e9d88
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-03-03 11:17:03 +0330

    ADD createOrder, getAllOrdersByUserId, getOrder, deleteOrder, updateOrderStatus & sendOrder middlewares

---
+++ controllers/orderController.js
@@ -0,0 +1,158 @@
+const asynchandler = require('express-async-handler');
+const uniqid = require('uniqid');
+const { logMiddleware, isObjectIdValid } = require('../utils/Api-Features');
+const Order = require('../models/orderModel');
+
+const getOrder = asynchandler(async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+    isObjectIdValid(orderId);
+
+    const order = await Order.findOne()
+      .where('_id')
+      .equals(orderId)
+      .populate('productId', 'title images')
+      .exec();
+
+    if (!order) throw new Error('Order Not Found!');
+
+    res.status(200).send({
+      orderId: order?._id,
+      products: order?.products.map((product) => {
+        return {
+          productId: product?.productId?._id,
+          title: product?.productId?.title,
+          images: product?.productId?.images,
+          quantity: product?.quantity,
+          color: product?.color,
+          price: product?.price,
+        };
+      }),
+      totalQuantity: order?.totalQuantity,
+      totalPrice: order?.totalPrice,
+      totalPayablePrice: order?.totalPayablePrice,
+      orderStatus: order?.orderStatus,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+const getAllOrdersByUserId = asynchandler(async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    const orders = await Order.find({ orderBy: userId })
+      .populate('productId', 'title images')
+      .exec();
+
+    if (orders.length === 0) throw new Error('Orders Not Found');
+
+    res.status(200).send(
+      orders.map((order) => {
+        return {
+          orderId: order?._id,
+          products: order?.products.map((product) => {
+            return {
+              productId: product?.productId?._id,
+              title: product?.productId?.title,
+              images: product?.productId?.images,
+              quantity: product?.quantity,
+              color: product?.color,
+              price: product?.price,
+            };
+          }),
+          totalQuantity: order?.totalQuantity,
+          totalPrice: order?.totalPrice,
+          totalPayablePrice: order?.totalPayablePrice,
+          orderStatus: order?.orderStatus,
+        };
+      })
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+const createOrder = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    const cart = req.cart;
+
+    const order = new Order({
+      products: cart?.products,
+      orderBy: userId,
+      totalQuantity: cart?.totalQuantity,
+      totalPrice: cart?.totalPrice,
+      totalPayablePrice: cart?.totalPayablePrice,
+      orderCode: uniqid(),
+      orderStatus: 'Not Processed',
+    });
+
+    await order.save();
+
+    req.order = order;
+
+    logMiddleware('createOrder');
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteOrder = asynchandler(async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+    isObjectIdValid(orderId);
+
+    const deletedOrder = await Order.findByIdAndUpdate(
+      orderId,
+      { softDelete: true },
+      { new: true }
+    );
+
+    if (!deletedOrder) throw new Error('Order Not Found!');
+
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const updateOrderStatus = asynchandler(async (req, res, next) => {
+  try {
+    const { status } = req.body;
+    const { orderId } = req.params;
+
+    isObjectIdValid(orderId);
+
+    const updateOrderStatus = await Order.findByIdAndUpdate(
+      orderId,
+      { orderStatus: status },
+      { new: true }
+    );
+
+    res.send(updateOrderStatus);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const sendOrder = asynchandler(async (req, res, next) => {
+  try {
+    const order = req.order;
+
+    res.status(201).send(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = {
+  createOrder,
+  getAllOrdersByUserId,
+  getOrder,
+  deleteOrder,
+  updateOrderStatus,
+  sendOrder,
+};
